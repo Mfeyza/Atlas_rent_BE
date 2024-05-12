9f9e6aa8cc2102ba47ef29162cecd5dcc0933523
M. Feyza Yıldırım <144602340+Mfeyza@users.noreply.github.com>
2024-05-12 14:50:03 +0300

    feat:CORS

--- index.js
+++ index.js
@@ -10,6 +10,10 @@ const PORT=process.env?.PORT || 8000
 require('express-async-errors')
 const {dbConnection}=require('./src/configs/dbConnection')
 dbConnection()
+app.use(cors({
+    origin: 'http://localhost:3000'
+}))
+
 app.use(express.json())
 app.use(require('./src/middlewares/authentication'))
 app.use(require('./src/middlewares/queryHandler'))
@@ -27,9 +31,6 @@ app.all('/', (req, res) => {
 })
 app.use(require('./src/routes/index'))
 app.use(require('./src/middlewares/errorHandler'))
-app.use(cors({
-    origin: 'http://localhost:3000'
-}))
 
 
 app.listen(PORT,()=> console.log(`http://:${PORT}`))
