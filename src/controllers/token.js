8f7559731549ff6e314b87456ceb33ecf7f3d952
M. Feyza Yıldırım <144602340+Mfeyza@users.noreply.github.com>
2024-05-21 13:22:41 +0300

    feat:live

--- src/controllers/token.js
+++ src/controllers/token.js
@@ -4,19 +4,7 @@ const { token } = require('morgan')
 const Token=require('../models/token')
 module.exports={
 list: async(req,res)=>{
-       /*
-            #swagger.tags = ["Tokens"]
-            #swagger.summary = "List Tokens"
-            #swagger.description = `
-                You can send query with endpoint for filter[], search[], sort[], page and limit.
-                <ul> Examples:
-                    <li>URL/?<b>filter[field1]=value1&filter[field2]=value2</b></li>
-                    <li>URL/?<b>search[field1]=value1&search[field2]=value2</b></li>
-                    <li>URL/?<b>sort[field1]=1&sort[field2]=-1</b></li>
-                    <li>URL/?<b>page=2&limit=1</b></li>
-                </ul>
-            `
-        */
+    
     const data= await res.getModelList(Token)
     res.status(200).send({
         error:false,
@@ -26,10 +14,7 @@ list: async(req,res)=>{
 },
 
 create : async(req,res)=>{
-     /*
-            #swagger.tags = ["Tokens"]
-            #swagger.summary = "Create Token"
-        */
+     
     const data=await Token.create(req.body)
     res.status(201).send({
         error:false,
@@ -37,11 +22,7 @@ create : async(req,res)=>{
     })
 },
 read: async (req,res)=>{
-      /*
-            #swagger.tags = ["Tokens"]
-            #swagger.summary = "Get Single Token"
-        */
-
+     
     const data= await Token.findOne({_id:req.params.id})
     res.status(200).send({
         error:false,
@@ -49,10 +30,7 @@ read: async (req,res)=>{
     })
 },
 update: async(req,res)=>{
-     /*
-            #swagger.tags = ["Tokens"]
-            #swagger.summary = "Update Token"
-        */
+   
 
     const data= await Token.updateOne({_id:req.params.id},req.body,{runValidators:true})
     res.status(202).send({
@@ -62,10 +40,7 @@ update: async(req,res)=>{
     })
 },
 deleteToken: async (req,res)=>{
-      /*
-            #swagger.tags = ["Tokens"]
-            #swagger.summary = "Delete Token"
-        */
+     
     const data= await Token.deleteOne({_id:req.params.id})
     res.status(data.deletedCount? 204 : 404).send({
         error: !data.deletedCount,
